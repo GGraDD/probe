522fdfa29294dcf238b4f3ba13fb163063047960
Mikhail Kirzhatskikh <frtstg@gmail.com>
2021-02-11 21:34:02 +0300

    выполнил первые 3 задания. Дальше - больше!)

---
+++ lesson_002/03_favorite_movies.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Есть строка с перечислением фильмов
+
+my_favorite_movies = 'Терминатор, Пятый элемент, Аватар, Чужие, Назад в будущее'
+
+# Выведите на консоль с помощью индексации строки, последовательно:
+#   первый фильм
+#   последний
+#   второй
+#   второй с конца
+
+# Переопределять my_favorite_movies и использовать .split() нельзя.
+# Запятая не должна выводиться.
+
+my_favorite_movies[0:10]+my_favorite_movies[-16:]+my_favorite_movies[11:25]+my_favorite_movies[-23:-17]
+
+print(my_favorite_movies[0:10]+my_favorite_movies[-16:]+my_favorite_movies[11:25]+my_favorite_movies[-23:-17])
